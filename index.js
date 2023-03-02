e05c875319201fb2980b3dd3dcc8272e02e3fc77
Karthik_M <karthik.mulagundmath@gokhana.com>
2023-03-02 16:55:14 +0530

    leet 26

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("set-up Git")
+var removeDuplicates = function(nums) {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+            continue;
+        }
+        nums[count] = nums[i];
+        count++;
+    }
+    return {count,nums};
+    // const set=new Set(nums);
+    // const resArray=Array.from(set)
+    // const removedLength=resArray.length
+    // const removeDuplicatesArrayLength=nums.length-resArray.length
+    // for(let i=0;i<removeDuplicatesArrayLength;i++){
+    //     resArray.push('_')
+    // }
+    // return resArray
+};
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
